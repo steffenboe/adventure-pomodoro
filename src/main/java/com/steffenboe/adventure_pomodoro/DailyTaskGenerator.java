839c0248b23c71ee8d8e7ec917397eaa381c69ac
Steffen Börner <steffen.boerner@exxeta.com>
2024-12-16 07:35:04 +0100

    Fix daily generation

--- src/main/java/com/steffenboe/adventure_pomodoro/DailyTaskGenerator.java
+++ src/main/java/com/steffenboe/adventure_pomodoro/DailyTaskGenerator.java
@@ -2,17 +2,15 @@ package com.steffenboe.adventure_pomodoro;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
-import java.util.Calendar;
-import java.util.Date;
 import java.util.List;
 import java.util.UUID;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import jakarta.annotation.PostConstruct;
 import reactor.core.publisher.Mono;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 @Service
 public class DailyTaskGenerator {
@@ -35,27 +33,37 @@ public class DailyTaskGenerator {
                     if (!dailyGeneration.lastGeneration().equals(LocalDate.now())) {
                         logger.info("Creating daily tasks...");
                         List<Todo> tasks = new ArrayList<>();
-                        tasks.add(new Todo(UUID.randomUUID().toString(), "Workout Session", false, "", List.of("New!")));
-                        tasks.add(new Todo(UUID.randomUUID().toString(), "Zahnseide benutzen", false, "", List.of("New!")));
+                        tasks.add(
+                                new Todo(UUID.randomUUID().toString(), "Workout Session", false, "", List.of("New!")));
+                        tasks.add(new Todo(UUID.randomUUID().toString(), "Zahnseide benutzen", false, "",
+                                List.of("New!")));
                         tasks.add(new Todo(UUID.randomUUID().toString(), "Tour planen", false, "", List.of("New!")));
-                        tasks.add(new Todo(UUID.randomUUID().toString(), "Bei einem Freund melden", false, "", List.of("New!")));
-                        tasks.add(new Todo(UUID.randomUUID().toString(), "Kühlschrank reinigen", false, "", List.of("New!")));
+                        tasks.add(new Todo(UUID.randomUUID().toString(), "Bei einem Freund melden", false, "",
+                                List.of("New!")));
+                        tasks.add(new Todo(UUID.randomUUID().toString(), "Kühlschrank reinigen", false, "",
+                                List.of("New!")));
                         tasks.add(new Todo(UUID.randomUUID().toString(), "Bad putzen", false, "", List.of("New!")));
                         tasks.add(new Todo(UUID.randomUUID().toString(), "Küche putzen", false, "", List.of("New!")));
-                        tasks.add(new Todo(UUID.randomUUID().toString(), "Wohnzimmer saugen", false, "", List.of("New!")));
+                        tasks.add(new Todo(UUID.randomUUID().toString(), "Wohnzimmer saugen", false, "",
+                                List.of("New!")));
                         tasks.add(new Todo(UUID.randomUUID().toString(), "Flur saugen", false, "", List.of("New!")));
                         tasks.add(new Todo(UUID.randomUUID().toString(), "Sarah küssen", false, "", List.of("New!")));
-                        int randomNumberOfTasks = (int) Math.random() * 4;
+
+                        int randomNumberOfTasks = (int) (Math.random() * 5);
+                        while (randomNumberOfTasks == 0) {
+                            randomNumberOfTasks = (int) (Math.random() * 5);
+                        }
+                        System.out.println(randomNumberOfTasks);
                         List<Todo> randomTasks = new ArrayList<>();
-                        for(int i = 0; i < randomNumberOfTasks; i++){
-                            int randomIndex = (int)(Math.random() * tasks.size());
+                        for (int i = 0; i < randomNumberOfTasks; i++) {
+                            int randomIndex = (int) (Math.random() * tasks.size());
                             randomTasks.add(tasks.get(randomIndex));
                             tasks.remove(randomIndex);
                         }
                         todoRepository.saveAll(randomTasks).subscribe();
-                    } 
+                    }
                     return dailyGenerationRepository.save(new DailyGeneration("1", LocalDate.now()))
-                                .map(x -> Mono.just(x));
+                            .map(x -> Mono.just(x));
                 }).subscribe();
     }
 }
